8748741df80c61318725ebda585d171737aff00e
Sophia <wwan18@my.bcit.ca>
2026-01-22 16:28:47 -0800

    tst2

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-/* function App() {
+function App() {
     const [count, setCount] = useState(0);
 
-    return (
+    /* return (
         <>
             <div>
                 <div
@@ -33,8 +33,7 @@ import "./App.css";
                 for some of my projects!
             </h3>
         </>
-    );
+    ); */
 }
 
 export default App;
-*/
